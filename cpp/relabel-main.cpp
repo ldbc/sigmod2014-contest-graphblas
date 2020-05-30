b0c23c2e184de3460afaf3803e2ceb89650ed20d
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-05-30 14:28:26 +0200

    Add print to make copying to google docs easy

--- cpp/relabel-main.cpp
+++ cpp/relabel-main.cpp
@@ -99,6 +99,7 @@ int main(int argc, char **argv) {
     double time2 = LAGraph_toc(tic);
     printf("Edge relabel time: %.2f\n", time2);
     fprintf(stderr, " Totally not usable value: %d\n", sum);
+    printf("%d\t%d\t%.2f\t%.2f\n", vertexMapperThreads, edgeMapperThreads, time1, time2);
 
     // Cleanup
     ok(LAGraph_finalize());

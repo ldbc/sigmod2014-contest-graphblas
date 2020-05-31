f682082e09935d13c1024a8e3822bb6b2ab424ad
Gabor Szarnyas <szarnyasg@gmail.com>
2020-05-31 10:52:21 +0200

    weird code

--- cpp/relabel-main.cpp
+++ cpp/relabel-main.cpp
@@ -1,61 +1,24 @@
 #include <iostream>
-#include <memory>
-#include <random>
-#include <filesystem>
 #include "gb_utils.h"
 #include "utils.h"
 #include "query-parameters.h"
 #include "relabel-common.h"
-
-// https://www.geeksforgeeks.org/binary-search/
-GrB_Index binarySearch(GrB_Index arr[], GrB_Index l, GrB_Index r, GrB_Index x)
-{
-    if (r >= l) {
-        GrB_Index mid = l + (r - l) / 2;
-
-        // If the element is present at the middle
-        // itself
-        if (arr[mid] == x)
-            return mid;
-
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-
-        // Else the element can only be present
-        // in right subarray
-        return binarySearch(arr, mid + 1, r, x);
-    }
-
-    // We reach here when element is not
-    // present in array
-    return -1;
-}
 
 int main(int argc, char **argv) {
     ok(LAGraph_init());
 
     GrB_Vector id2index = NULL;
 
-    // TODO: set threads here
-    constexpr uint64_t nthreads = 12;
+    constexpr uint64_t nthreads = 1;
     LAGraph_set_nthreads (nthreads) ;
 
     // prepare array of IDs
     srand(0);
     constexpr uint64_t idsSeed = 50U;
 
-    // std::vector<GrB_Index> vertex_ids = getVertexIds(nnodes);
-    // std::vector<GrB_Index> edge_srcs = getListOfIds(vertex_ids, 131, nedges, nthreads);
-    // std::vector<GrB_Index> edge_trgs = getListOfIds(vertex_ids, 199, nedges, nthreads);
-
-    // saveToFile(vertex_ids, "5_15_vertex.data");
-    // saveToFile(edge_srcs, "5_15_edge_srcs.data");
-    // saveToFile(edge_trgs, "5_15_edge_trgs.data");
-    
     std::vector<GrB_Index> vertex_ids = readFromFile("vertex.data");
     std::vector<GrB_Index> edge_srcs = readFromFile("edge_srcs.data");
+    std::vector<GrB_Index> edge_srcs2 = readFromFile("edge_srcs.data");
     std::vector<GrB_Index> edge_trgs = readFromFile("edge_trgs.data");
 
     printf("Reading is done\n");
@@ -72,36 +35,15 @@ int main(int argc, char **argv) {
     double time1 = LAGraph_toc(tic);
     printf("Vertex relabel time: %.2f\n", time1);
 
-    /*
-    printf("I: ");
-    for (GrB_Index i = 0; i < nnodes; i++) printf("%d, ", I[i]);
-    printf("\n");
 
-    printf("X: ");
-    for (GrB_Index i = 0; i < nnodes; i++) printf("%d, ", X[i]);
-    printf("\n");
-     */
+    printf("stl: %ld  \n", std::lower_bound(I, I + nnodes, edge_srcs[0]) - I   );
+    printf("stl: %ld  \n", std::lower_bound(I, I + nnodes, edge_srcs[1]) - I   );
+    printf("stl: %ld  \n", std::lower_bound(I, I + nnodes, edge_srcs[2]) - I   );
 
-    // just to prevent compiler to optimize out
-    GrB_Index sum = 0u;
-    // remap edges
-    LAGraph_tic (tic);
-#pragma omp parallel for num_threads(nthreads) schedule(static)
-    for (GrB_Index j = 0; j < nedges; j++) {
-        GrB_Index src_index, trg_index;
-        // GrB_Vector_extractElement_UINT64(&src_index, id2index, edge_srcs[j]);
-        // GrB_Vector_extractElement_UINT64(&trg_index, id2index, edge_trgs[j]);
-        src_index = X[binarySearch(I, 0, nnodes, edge_srcs[j])];
-        trg_index = X[binarySearch(I, 0, nnodes, edge_trgs[j])];
-        sum += src_index + trg_index;
-        // printf("%d -> %d ==> %d -> %d\n", edge_srcs[j], edge_trgs[j], src_index, trg_index);
+    for (int idx = 0; idx < 3; idx++) {
+        printf("stl: %ld  \n", std::lower_bound(I, I + nnodes, edge_srcs2[idx]) - I );
     }
-    double time2 = LAGraph_toc(tic);
-    printf("Edge relabel time: %.2f\n", time2);
-    fprintf(stderr, " Totally not usable value: %ld\n", sum);
-    printf("LAGraph with custom binsearch,%ld,%ld,%.2f,%.2f\n", nthreads, nthreads, time1, time2);
 
-    // Cleanup
     ok(LAGraph_finalize());
 
     return 0;

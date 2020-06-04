03362a84e325027db9cec4a93019a3f1c06230f1
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-04 22:40:21 +0200

    grb_index -> uint64

--- cpp/relabel-main.cpp
+++ cpp/relabel-main.cpp
@@ -20,17 +20,17 @@ int main(int argc, char **argv) {
     srand(0);
     constexpr uint64_t idsSeed = 50U;
 
-    // std::vector<GrB_Index> vertex_ids = getVertexIds(nnodes);
-    // std::vector<GrB_Index> edge_srcs = getListOfIds(vertex_ids, 131, nedges, nthreads);
-    // std::vector<GrB_Index> edge_trgs = getListOfIds(vertex_ids, 199, nedges, nthreads);
+    // std::vector<uint64_t> vertex_ids = getVertexIds(nnodes);
+    // std::vector<uint64_t> edge_srcs = getListOfIds(vertex_ids, 131, nedges, nthreads);
+    // std::vector<uint64_t> edge_trgs = getListOfIds(vertex_ids, 199, nedges, nthreads);
 
     // saveToFile(vertex_ids, "5_15_vertex.data");
     // saveToFile(edge_srcs, "5_15_edge_srcs.data");
     // saveToFile(edge_trgs, "5_15_edge_trgs.data");
     
-    std::vector<GrB_Index> vertex_ids = readFromFile("vertex.data");
-    std::vector<GrB_Index> edge_srcs = readFromFile("edge_srcs.data");
-    std::vector<GrB_Index> edge_trgs = readFromFile("edge_trgs.data");
+    std::vector<uint64_t> vertex_ids = readFromFile("vertex.data");
+    std::vector<uint64_t> edge_srcs = readFromFile("edge_srcs.data");
+    std::vector<uint64_t> edge_trgs = readFromFile("edge_trgs.data");
 
     printf("Reading is done\n");
 
@@ -39,30 +39,30 @@ int main(int argc, char **argv) {
     // build id <-> index mapping
     LAGraph_tic (tic);
     LAGraph_dense_relabel(NULL, NULL, &id2index, vertex_ids.data(), nnodes, NULL);
-    GrB_Index* I = (GrB_Index*) LAGraph_malloc(nnodes, sizeof(GrB_Index));
-    GrB_Index* X = (GrB_Index*) LAGraph_malloc(nnodes, sizeof(GrB_Index));
-    GrB_Index nnodes2 = nnodes;
+    uint64_t* I = (uint64_t*) LAGraph_malloc(nnodes, sizeof(uint64_t));
+    uint64_t* X = (uint64_t*) LAGraph_malloc(nnodes, sizeof(uint64_t));
+    uint64_t nnodes2 = nnodes;
     ok(GrB_Vector_extractTuples_UINT64(I, X, &nnodes2, id2index));
     double time1 = LAGraph_toc(tic);
     printf("Vertex relabel time: %.2f\n", time1);
 
     /*
     printf("I: ");
-    for (GrB_Index i = 0; i < nnodes; i++) printf("%d, ", I[i]);
+    for (uint64_t i = 0; i < nnodes; i++) printf("%d, ", I[i]);
     printf("\n");
 
     printf("X: ");
-    for (GrB_Index i = 0; i < nnodes; i++) printf("%d, ", X[i]);
+    for (uint64_t i = 0; i < nnodes; i++) printf("%d, ", X[i]);
     printf("\n");
      */
 
     // just to prevent compiler to optimize out
-    GrB_Index sum = 0u;
+    uint64_t sum = 0u;
     // remap edges
     LAGraph_tic (tic);
 #pragma omp parallel for num_threads(nthreads) schedule(static)
-    for (GrB_Index j = 0; j < nedges; j++) {
-        GrB_Index src_index, trg_index;
+    for (uint64_t j = 0; j < nedges; j++) {
+        uint64_t src_index, trg_index;
 //         GrB_Vector_extractElement_UINT64(&src_index, id2index, edge_srcs[j]);
 //         GrB_Vector_extractElement_UINT64(&trg_index, id2index, edge_trgs[j]);
 

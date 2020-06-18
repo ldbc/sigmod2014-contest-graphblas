f82afc7ec4360427f252ee1cb84cd94956249e97
marci543 <marci543@users.noreply.github.com>
2020-06-18 21:44:05 +0200

    Fix path during Python measurements

--- python/run_py_with_param.sh
+++ python/run_py_with_param.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-PARAMS="${@:4}"
+set -e
 
-python3 main.py --data_path $1 --queries_to_run $3 --mode with_param --query_args  ${PARAMS// /,}
+DATA_PATH=$(realpath "$1")
+
+# https://stackoverflow.com/a/9057392
+# https://github.com/koalaman/shellcheck/wiki/SC2124#correct-code
+PARAMS=( "${@:4}" )
+
+PYTHON_DIR=$(dirname "$0")
+
+# run Python script from python/ folder
+cd "$PYTHON_DIR"
+
+python3 main.py --data_path "$DATA_PATH" --queries_to_run $3 --mode with_param --query_args "${PARAMS// /,}"

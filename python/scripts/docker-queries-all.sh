4569756150aeb4f98dd727feed6f87d35abeed06
marci543 <marci543@users.noreply.github.com>
2020-06-20 17:47:06 +0200

    Allow command line arguments for Python runner in Docker

--- python/scripts/docker-queries-all.sh
+++ python/scripts/docker-queries-all.sh
@@ -2,4 +2,4 @@
 docker run --rm --user root -e NB_UID=$(id -u) -e NB_GID=$(id -g) \
   -v "$(pwd)":/home/jovyan/sigmod2014-pc \
   -it graphblas/pygraphblas-notebook:custom \
-  bash -c 'cd ~/sigmod2014-pc/python ; python3 main.py --mode from_file'
+  bash -c "cd ~/sigmod2014-pc/python ; python3 main.py --mode from_file $*"

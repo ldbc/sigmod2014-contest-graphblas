82565785bf9583474bd7ae674dec6b108bdfe6a4
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-25 01:02:27 +0200

    unsuccessful attempt at optimizing Q1

--- cpp/Query1.h
+++ cpp/Query1.h
@@ -18,47 +18,107 @@ class Query1 : public Query<uint64_t, uint64_t, int> {
     GrB_Index p1, p2;
     int comment_lower_limit;
 
-    std::tuple<std::string, std::string> initial_calculation() override {
-
-        GrB_Matrix A;
-
-        GBxx_Object<GrB_Matrix> personAToComment2 = GB(GrB_Matrix_new, GrB_UINT64, input.persons.size(),
-                                                       input.comments.size());
-
-        ok(GrB_mxm(personAToComment2.get(), GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, input.hasCreator.matrix.get(),
-                   input.replyOf.matrix.get(), GrB_DESC_T0));
-        // ok(GxB_Matrix_fprint(personAToComment2.get(), "personAToComment2", GxB_SUMMARY, stdout));
-
-        GBxx_Object<GrB_Matrix> personToPerson = GB(GrB_Matrix_new, GrB_UINT64, input.persons.size(),
-                                                    input.persons.size());
-
-        ok(GrB_mxm(personToPerson.get(), input.knows.matrix.get(), GrB_NULL, GxB_PLUS_TIMES_INT64,
-                   personAToComment2.get(), input.hasCreator.matrix.get(), GrB_NULL));
-        // ok(GxB_Matrix_fprint(personToPerson.get(), "personToPerson", GxB_SUMMARY, stdout));
-
+    void advance_wavefront(GrB_Matrix HasCreator, GrB_Matrix ReplyOf,  GrB_Matrix Knows, GrB_Vector frontier, GrB_Vector next, GrB_Vector seen, GrB_Index n) {
         if (comment_lower_limit == -1) {
-            A = input.knows.matrix.get();
-
+            ok(GrB_vxm(next, seen, NULL, GxB_ANY_PAIR_BOOL, frontier, Knows, GrB_DESC_RC));
         } else {
-            auto limit = GB(GxB_Scalar_new, GrB_INT32);
-            ok(GxB_Scalar_setElement_INT32(limit.get(), comment_lower_limit));
-            ok(GxB_Matrix_select(personToPerson.get(), GrB_NULL, GrB_NULL, GxB_GT_THUNK, personToPerson.get(),
-                                 limit.get(), GrB_NULL));
-            // make symmetric
-            ok(GrB_eWiseMult_Matrix_BinaryOp(personToPerson.get(), GrB_NULL, GrB_NULL,
-                                             GxB_PAIR_UINT64, personToPerson.get(), personToPerson.get(), GrB_DESC_T0));
+            auto limit = GB(GxB_Scalar_new, GrB_INT64);
+            ok(GxB_Scalar_setElement_INT64(limit.get(), comment_lower_limit));
 
-            A = personToPerson.get();
-        }
+            // build selection matrix based on the frontier's content
+            GrB_Matrix Sel;
+            GrB_Matrix_new(&Sel, GrB_BOOL, n, n);
+
+            GrB_Index nvals1;
+            GrB_Vector_nvals(&nvals1, frontier);
+            GrB_Index *I = (GrB_Index*) (LAGraph_malloc(nvals1, sizeof(GrB_Index)));
+            bool *X = (bool*) (LAGraph_malloc(nvals1, sizeof(bool)));
+
+            GrB_Index nvals2 = nvals1;
+            ok(GrB_Vector_extractTuples_BOOL(I, X, &nvals2, frontier));
+            assert(nvals1 == nvals2);
+            GrB_Matrix_build_BOOL(Sel, I, I, X, nvals1, GrB_LOR);
+
+            GrB_Matrix FilteredHasCreatorTransposed;
+            ok(GrB_Matrix_new(&FilteredHasCreatorTransposed, GrB_BOOL, n, input.comments.size()));
+            GrB_mxm(FilteredHasCreatorTransposed, NULL, NULL, GxB_ANY_PAIR_BOOL, Sel, HasCreator, GrB_DESC_T1);
+
+            // direction 1
+            GrB_Matrix FilteredHasCreatorTransposedReplyOf;
+            ok(GrB_Matrix_new(&FilteredHasCreatorTransposedReplyOf, GrB_UINT64, n, input.comments.size()));
+            ok(GrB_mxm(FilteredHasCreatorTransposedReplyOf, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_UINT64, FilteredHasCreatorTransposed, ReplyOf, NULL));
+
+            GrB_Matrix Interactions1;
+            ok(GrB_Matrix_new(&Interactions1, GrB_UINT64, n, n));
+            ok(GrB_mxm(Interactions1, Knows, NULL, GrB_PLUS_TIMES_SEMIRING_UINT64, FilteredHasCreatorTransposedReplyOf, HasCreator, NULL));
+
 
 #ifndef NDEBUG
-        ok(GxB_Matrix_fprint(A, "personToPersonFiltered", GxB_SUMMARY, stdout));
+//            GxB_Matrix_fprint(Interactions1, "Interactions1 before select", GxB_SUMMARY, stdout);
+#endif
+//            ok(GxB_Matrix_select(Interactions1, NULL, NULL, GxB_GT_THUNK, Interactions1, limit.get(), NULL));
+#ifndef NDEBUG
+//            GxB_Matrix_fprint(Interactions1, "Interactions1 after select", GxB_SUMMARY, stdout);
+#endif
+
+            // direction 2
+            GrB_Matrix FilteredHasCreatorTransposedReplyOfT;
+            ok(GrB_Matrix_new(&FilteredHasCreatorTransposedReplyOfT, GrB_UINT64, n, input.comments.size()));
+            ok(GrB_mxm(FilteredHasCreatorTransposedReplyOfT, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_UINT64, FilteredHasCreatorTransposed, ReplyOf, GrB_DESC_T1));
+
+
+            GrB_Matrix Interactions2;
+            ok(GrB_Matrix_new(&Interactions2, GrB_UINT64, n, n));
+            ok(GrB_mxm(Interactions2, Interactions1, GrB_NULL, GrB_PLUS_TIMES_SEMIRING_UINT64, FilteredHasCreatorTransposedReplyOfT, HasCreator, NULL));
+
+
+#ifndef NDEBUG
+            GxB_Matrix_fprint(Interactions2, "Interactions2 before select", GxB_SUMMARY, stdout);
+#endif
+#ifndef NDEBUG
+//            GxB_Matrix_fprint(Interactions2, "Interactions2 after select", GxB_SUMMARY, stdout);
 #endif
 
 
+//#ifndef NDEBUG
+//            GxB_Matrix_fprint(Sel, "Sel", GxB_SUMMARY, stdout);
+//            GxB_Matrix_fprint(FilteredHasCreatorTransposed, "FilteredHasCreatorTransposed", GxB_SUMMARY, stdout);
+//            GxB_Matrix_fprint(FilteredHasCreatorTransposedReplyOf, "FilteredHasCreatorTransposedReplyOf", GxB_SUMMARY, stdout);
+//            GxB_Matrix_fprint(FilteredHasCreatorTransposedReplyOfT, "FilteredHasCreatorTransposedReplyOfT", GxB_SUMMARY, stdout);
+//            GxB_Matrix_fprint(Interactions1, "Interactions1", GxB_SUMMARY, stdout);
+//            GxB_Matrix_fprint(Interactions2, "Interactions2", GxB_SUMMARY, stdout);
+//#endif
+
+            // Interactions1 = Interactions1 * Interactions2
+            ok(GrB_Matrix_eWiseMult_BinaryOp(Interactions1, NULL, NULL, GrB_MIN_UINT64, Interactions1, Interactions2, NULL));
+            ok(GxB_Matrix_select(Interactions1, NULL, NULL, GxB_GT_THUNK, Interactions1, limit.get(), NULL));
+#ifndef NDEBUG
+//            GxB_Matrix_fprint(Interactions1, "Interactions final", GxB_SUMMARY, stdout);
+#endif
+            ok(GrB_Matrix_reduce_BinaryOp(next, NULL, NULL, GxB_PAIR_BOOL, Interactions1, GrB_DESC_T0));
+
+        }
+    }
+
+    std::tuple<std::string, std::string> initial_calculation() override {
+
+#ifndef NDEBUG
+        using namespace std::chrono;
+        auto start1 = high_resolution_clock::now();
+#endif
+
+        GrB_Matrix A = input.knows.matrix.get();
+
+#ifndef NDEBUG
+        auto duration1 = round<microseconds>(high_resolution_clock::now() - start1);
+        auto start2 = high_resolution_clock::now();
+
+        ok(GxB_Matrix_fprint(A, "personToPersonFiltered", GxB_SUMMARY, stdout));
+#endif
 
         GrB_Index n;
         ok(GrB_Matrix_nrows(&n, A));
+
 
         GBxx_Object<GrB_Vector> frontier1 = GB(GrB_Vector_new, GrB_BOOL, n);
         GBxx_Object<GrB_Vector> frontier2 = GB(GrB_Vector_new, GrB_BOOL, n);
@@ -76,9 +136,8 @@ class Query1 : public Query<uint64_t, uint64_t, int> {
         if (p1 == p2) {
             distance = 0;
         } else {
-            // use two "push" frontiers
             for (GrB_Index level = 1; level < n / 2 + 1; level++) {
-                ok(GrB_vxm(next1.get(), seen1.get(), NULL, GxB_ANY_PAIR_BOOL, frontier1.get(), A, GrB_DESC_RC));
+                advance_wavefront(input.hasCreator.matrix.get(), input.replyOf.matrix.get(), input.knows.matrix.get(), frontier1.get(), next1.get(), seen1.get(), n);
 
                 GrB_Index next1nvals;
                 ok(GrB_Vector_nvals(&next1nvals, next1.get()));
@@ -96,7 +155,9 @@ class Query1 : public Query<uint64_t, uint64_t, int> {
                     break;
                 }
 
-                ok(GrB_vxm(next2.get(), seen2.get(), NULL, GxB_ANY_PAIR_BOOL, frontier2.get(), A, GrB_DESC_RC));
+//                ok(GrB_vxm(next2.get(), seen2.get(), NULL, GxB_ANY_PAIR_BOOL, frontier2.get(), A, GrB_DESC_RC));
+                advance_wavefront(input.hasCreator.matrix.get(), input.replyOf.matrix.get(), input.knows.matrix.get(), frontier2.get(), next2.get(), seen2.get(), n);
+
                 ok(GrB_Vector_eWiseMult_BinaryOp(intersection2.get(), NULL, NULL, GrB_LAND, next1.get(), next2.get(), NULL));
 
                 GrB_Index intersection2_nvals;
@@ -122,6 +183,10 @@ class Query1 : public Query<uint64_t, uint64_t, int> {
         }
 
 #ifndef NDEBUG
+        auto duration2 = round<microseconds>(high_resolution_clock::now() - start2);
+        std::cout << "induced subgraph: " << duration1.count()
+                  << " bfs: "             << duration2.count() <<'\n';
+
 //        ok(GxB_Vector_fprint(v_output.get(), "output_vec", GxB_SUMMARY, stdout));
 #endif
 

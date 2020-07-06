ce7a78f883d34bf74c6c2bf278fc4baa76bde0dc
marci543 <marci543@users.noreply.github.com>
2020-07-06 17:34:48 +0200

    Use custom Docker image

--- python/scripts/docker-queries-all.sh
+++ python/scripts/docker-queries-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker run --rm --user root -e NB_UID=$(id -u) -e NB_GID=$(id -g) \
   -v "$(pwd)":/home/jovyan/sigmod2014-pc \
-  -it graphblas/pygraphblas-notebook:custom \
+  -it ftsrg/pygraphblas-notebook \
   bash -c "cd ~/sigmod2014-pc/python ; python3 main.py --mode from_file $*"

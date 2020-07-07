292e61811c86aca71b5e3ff5bc0cbffd87c4cc56
marci543 <marci543@users.noreply.github.com>
2020-07-07 16:41:41 +0200

    Add C++ tests to CI

---
+++ cpp/scripts/rebuild.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# fail fast, echo command
+set -e -x
+
+# Build type: first argument, BUILD_TYPE env.var., "Release" (first existing option)
+BUILD_TYPE=${1:-${BUILD_TYPE:-Release}}
+BUILD_TYPE_LOWERCASE=$(echo $BUILD_TYPE | tr '[:upper:]' '[:lower:]')
+PRINT_RESULTS=${PRINT_RESULTS:-1}
+CPP_DIR=$(dirname "$0")/..
+CMAKE_BUILD_DIR=cmake-build-$BUILD_TYPE_LOWERCASE
+
+cd "$CPP_DIR"
+rm -rf "$CMAKE_BUILD_DIR"
+mkdir "$CMAKE_BUILD_DIR"
+cd "$CMAKE_BUILD_DIR"
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPRINT_RESULTS=$PRINT_RESULTS ..
+make -j$(nproc)
